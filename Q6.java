fab77dec93fdcb756a75ea4a27075f9e34e4b6ab
RemiViez <remi.viez@etu.univ-lehavre.fr>
2025-10-08 13:24:38 +0000

    modification

--- Q6.java
+++ Q6.java
@@ -7,7 +7,7 @@ public class Q6 {
         int n = Integer.parseInt(args[0]);
         for(int i = n; i >= 1; i--) {
             for(int j = 1; j <= i; j++) {
-                System.out.print("x");
+                System.out.print("o");
             }
             System.out.println();
         }
